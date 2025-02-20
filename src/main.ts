45a99bc9285d94c1b45324452b82acd7b9e4a7cc
kevinkeyssx <keyss1700@gmail.com>
2025-02-20 18:46:17 -0300

    Swagger enabled in /docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
-import { NestFactory } from '@nestjs/core';
+import {
+	DocumentBuilder,
+	SwaggerModule
+} 									from '@nestjs/swagger';
+import { NestFactory }              from '@nestjs/core';
+import { Logger  }   from '@nestjs/common';
+
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
-}
-bootstrap();
+
+( async () => {
+    const logger    = new Logger( 'Main' );
+    const app       = await NestFactory.create( AppModule );
+
+
+
+    const config = new DocumentBuilder()
+		.setTitle( 'Pet Social Network Documentation' )
+		.setDescription( 'Pet Social Network API Documentation' )
+		.setVersion( '1.0' )
+		.build();
+
+	const document = SwaggerModule.createDocument( app, config );
+	SwaggerModule.setup( 'docs', app, document );
+
+    await app.listen( process.env.PORT ?? 3000 );
+	logger.log( `Application is running on: ${ process.env.HOST_API }:${ process.env.PORT_API }`);
+})();
