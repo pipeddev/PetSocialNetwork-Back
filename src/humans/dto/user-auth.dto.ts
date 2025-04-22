18f5aceab8ce62da7a6416ac69d417aa134f9f5e
kevinkeyssx <keyss1700@gmail.com>
2025-04-21 23:19:56 -0400

    Omit remove

--- src/humans/dto/user-auth.dto.ts
+++ src/humans/dto/user-auth.dto.ts
@@ -1,6 +1,4 @@
-import { CreateHumanDto } from '@humans/dto/create-human.dto';
-
 import { HumanDto } from './human.entity';
 
 
-export class HumanAuthDto extends HumanDto implements Omit<CreateHumanDto, 'password'> {}
+export class HumanAuthDto extends HumanDto {}
