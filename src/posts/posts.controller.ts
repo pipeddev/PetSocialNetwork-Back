b50e0e3bca4cdb5f6eec5f3d51128e3841160429
kevinkeyssx <keyss1700@gmail.com>
2025-04-21 23:20:28 -0400

    Tabs 2

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -19,58 +19,58 @@ import { CreatePostDto } from '@posts/dto/create-post.dto';
 import { UpdatePostDto } from '@posts/dto/update-post.dto';
 import { HumanAuthDto } from '@humans/dto/user-auth.dto';
 
-@ApiTags('Posts') 
+@ApiTags('Posts')
 @Controller('posts')
 @ApiBearerAuth('access-token')
 @UseGuards( AuthGuard )
 export class PostsController {
-  constructor( private readonly postsService: PostsService ) {}
+	constructor( private readonly postsService: PostsService ) {}
 
-  @Post()
-  create(
+	@Post()
+	create(
 		@Param('petId', ParseUUIDPipe ) petId: string,
 		@Body() createPostDto: CreatePostDto
 	) {
-    return this.postsService.create( petId, createPostDto );
+		return this.postsService.create( petId, createPostDto );
 	}
 
-  @Get('by-pet/:petId')
- 	@ApiOkResponse({ description: 'List of posts' })
-  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
-  findAllByPet(
+	@Get('by-pet/:petId')
+	@ApiOkResponse({ description: 'List of posts' })
+	@ApiUnauthorizedResponse({ description: 'Unauthorized' })
+	findAllByPet(
 		@CurrentHuman() human: HumanAuthDto,
 		@Param( 'petId', ParseUUIDPipe ) petId: string,
 	) {
-    return this.postsService.findAllByPet( human, petId );
-  }
+		return this.postsService.findAllByPet( human, petId );
+	}
 
 	@Get('index/:petId')
 	@ApiOkResponse({ description: 'List of posts' })
-  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
-  findAllIndex(
+	@ApiUnauthorizedResponse({ description: 'Unauthorized' })
+	findAllIndex(
 		@CurrentHuman() human: HumanAuthDto,
 		@Param( 'petId', ParseUUIDPipe ) petId: string,
 	) {
-    return this.postsService.findAllIndex( human, petId );
-  }
+		return this.postsService.findAllIndex( human, petId );
+	}
 
-  @Get( ':id' )
-  findOne(
+	@Get( ':id' )
+	findOne(
 		@Param( 'id', ParseUUIDPipe ) id: string
 	) {
-    return this.postsService.findOne( id );
-  }
-
-  @Patch(':id')
-  update(
+		return this.postsService.findOne( id );
+	}
+	
+	@Patch(':id')
+	update(
 		@Param('id', ParseUUIDPipe) id: string,
 		@Body() updatePostDto: UpdatePostDto
 	) {
-    return this.postsService.update( id, updatePostDto );
-  }
-
-  @Delete(':id')
-  remove(@Param('id', ParseUUIDPipe) id: string) {
-    return this.postsService.remove( id );
-  }
+		return this.postsService.update( id, updatePostDto );
+	}
+	
+	@Delete(':id')
+	remove(@Param('id', ParseUUIDPipe) id: string) {
+		return this.postsService.remove( id );
+	}
 }
