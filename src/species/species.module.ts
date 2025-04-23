5cd8a6a1a88fe078fbf8fbcfb954d5dccc07fc9e
kevinkeyssx <keyss1700@gmail.com>
2025-04-22 20:37:53 -0400

    Auth module enabled

--- src/species/species.module.ts
+++ src/species/species.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 
+import { AuthModule } from '@auth/auth.module';
 import { SpeciesService }			from '@species/species.service';
 import { SpeciesController } 	from '@species/species.controller';
 
 @Module({
-  controllers: [SpeciesController],
-  providers: [SpeciesService],
+  controllers	: [SpeciesController],
+  providers		: [SpeciesService],
+	imports			: [AuthModule]
+
 })
 export class SpeciesModule {}
