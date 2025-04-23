643005c6eeb207f940afd17cf936f380071b18bf
kevinkeyssx <keyss1700@gmail.com>
2025-04-22 20:35:33 -0400

    Comment cotroller valid token & commentId & postId & parentCommentId

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -1,34 +1,55 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { CommentsService } from './comments.service';
-import { CreateCommentDto } from './dto/create-comment.dto';
-import { UpdateCommentDto } from './dto/update-comment.dto';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, ParseUUIDPipe } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
+import { AuthGuard }				from '@auth/guards/auth.guard';
+import { CurrentHuman }			from '@auth/decorators/current-human.decorator';
+import { CommentsService }	from '@comments/comments.service';
+import { CreateCommentDto } from '@comments/dto/create-comment.dto';
+import { UpdateCommentDto } from '@comments/dto/update-comment.dto';
+import { HumanAuthDto }			from '@humans/dto/user-auth.dto';
+
+@ApiTags('Comments')
 @Controller('comments')
+@ApiBearerAuth('access-token')
+@UseGuards( AuthGuard )
 export class CommentsController {
-  constructor(private readonly commentsService: CommentsService) {}
+  constructor( private readonly commentsService: CommentsService ) {}
 
   @Post()
-  create(@Body() createCommentDto: CreateCommentDto) {
-    return this.commentsService.create(createCommentDto);
+  create(
+		@Body() createCommentDto: CreateCommentDto
+	) {
+    return this.commentsService.create( createCommentDto );
   }
 
-  @Get()
-  findAll() {
-    return this.commentsService.findAll();
+  @Get(':postId')
+  findCommentsByPost(
+		@Param( 'postId', ParseUUIDPipe ) id: string,
+	) {
+    return this.commentsService.findCommentsByPost( id );
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.commentsService.findOne(+id);
+	@Get('/replies/:commentId')
+  findRepliesByComments(
+		@Param( 'commentId', ParseUUIDPipe ) id: string,
+	) {
+    return this.commentsService.findRepliesByComments( id );
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCommentDto: UpdateCommentDto) {
-    return this.commentsService.update(+id, updateCommentDto);
-  }
+  update(
+		@Param( 'id', ParseUUIDPipe ) id: string,
+		@Body() updateCommentDto: UpdateCommentDto,
+		@CurrentHuman() human: HumanAuthDto,
+	) {
+		return this.commentsService.update( id, updateCommentDto, human );
+	}
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.commentsService.remove(+id);
+  remove(
+		@Param( 'id', ParseUUIDPipe ) id: string,
+		@CurrentHuman() human: HumanAuthDto,
+	) {
+    return this.commentsService.remove( id, human );
   }
 }
