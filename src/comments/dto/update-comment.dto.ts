d9e7554ee80a184ed4f6b0662736513f87a90277
kevinkeyssx <keyss1700@gmail.com>
2025-04-22 20:37:12 -0400

    Update comment DTO added

--- src/comments/dto/update-comment.dto.ts
+++ src/comments/dto/update-comment.dto.ts
@@ -1,4 +1,14 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateCommentDto } from './create-comment.dto';
+import { ApiProperty } from "@nestjs/swagger";
 
-export class UpdateCommentDto extends PartialType(CreateCommentDto) {}
+import { IsString } from "class-validator";
+
+export class UpdateCommentDto {
+
+	@ApiProperty({
+		description	: 'The content of the comment',
+		example			: 'This is the content of my first comment',
+	})
+	@IsString()
+	content: string;
+
+}
