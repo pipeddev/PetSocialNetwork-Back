1e35c4131110d23bea4eff793e326ac7a3c75e15
kevinkeyssx <keyss1700@gmail.com>
2025-04-22 20:35:48 -0400

    AuthModules enabled

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+
+import { AuthModule } from '@auth/auth.module';
 import { CommentsService } from './comments.service';
 import { CommentsController } from './comments.controller';
 
 @Module({
-  controllers: [CommentsController],
-  providers: [CommentsService],
+  controllers	: [CommentsController],
+  providers		: [CommentsService],
+	imports			: [AuthModule]
 })
 export class CommentsModule {}
